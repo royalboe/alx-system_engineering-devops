4f902141fb3a530a3e1fab7d7ec4d9a70eb779d4
Royalboe <auomidejohnson@gmail.com>
2022-09-18 10:34:35 +0000

    fixed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 include stdlib
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/holberton',
+  line    => '    IdentityFile ~/.ssh/school',
   replace => true,
 }
 
