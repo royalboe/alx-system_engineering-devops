19aff0c6d79893448d27b106e14bc6c508c85af6
Royalboe <auomidejohnson@gmail.com>
2022-11-02 02:58:29 +0000

    fixing bugs

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,52 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
-import requests
+"""
+module for function to parses the title of all hot articles, and prints
+a sorted count of given keywords (case-insensitive, delimited by spaces
+"""
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+def count_words(subreddit, word_list, instances={}, count=0, after=None):
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
+    Args:
+        subreddit: subreddit name
+        word_list: a list containing the keywords of all hot articles titles
+        instances: a dictionary containing the keywords and their frequency
+        count: counter to change prototype
+        after: to validate subreddit
+
+    Returns:
+        returns a dictionary containing the frequency of certain
+        keywords in the hot articles
+    """
+    import requests
+
+    headers = {'User-Agent': 'User-Agent'}
+    parameters = {'after': after,
+                  'count': count}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=headers, params=parameters,
                             allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+    if response.status_code == 200:
+        results = response.json().get("data")
+        after = results.get("after")
+        count += results.get("dist")
+        for child in results.get("children"):
+            hot_list = child.get("data").get("title").lower().split()
+            for word in word_list:
+                if word.lower() in hot_list:
+                    times = len([article for article in hot_list if
+                                article == word.lower()])
+                    if instances.get(word.lower()) is None:
+                        instances[word.lower()] = times
+                    else:
+                        instances[word.lower()] += times
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
-
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+        if after is None:
+            if len(instances) == 0:
+                return
+            instances = sorted(instances.items(),
+                               key=lambda kv: (-kv[1], kv[0]))
+            [print("{}: {}".format(k, v)) for k, v in instances]
+        else:
+            count_words(subreddit, word_list, instances, count, after)
+    return
