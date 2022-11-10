a00c735afe101f49975182f81116b56c5a1d3bee
Royalboe <auomidejohnson@gmail.com>
2022-11-10 23:20:55 +0100

    postgres

--- ansible/postgres.sh
+++ ansible/postgres.sh
@@ -19,3 +19,18 @@ sudo chsh postgres -s /bin/bash
 echo "Start postgresql service"
 
 sudo systemctl start postgresql.service
+
+echo " Create a new role for postgres"
+sudo -u postgres createuser --goodybag
+sudo -i -u postgres psql -c "CREATE USER goodybag WITH ENCRYPTED PASSWORD 'goodybag'"
+
+echo "Create a new database"
+sudo -i -u postgres psql -c "CREATE DATABASE laraveldb WITH ENCODING 'UTF8' TEMPLATE template0"
+
+echo "Grant user with privilege over db"
+sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE laraveldb TO goodybag"
+
+echo 'Configure user login method'
+echo -e 'local\tall\t\goodybag\t\t\t\tmd5' >> /etc/postgresql/15/main/pg_hba.conf
+
+sudo systemctl restart postgresql.service
